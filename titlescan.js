212263d0a93176be3b87b6f8569cb4310aca67b2
14muffinz <135548153+14muffinz@users.noreply.github.com>
2026-04-25 21:14:32 -0400

    Update titlescan.js

--- titlescan.js
+++ titlescan.js
@@ -2,7 +2,30 @@ const links = document.querySelectorAll('.blurb a');
 
 for (var link of links) {
     if (link.textContent.trim().toLowerCase().includes('ai generated')) {
-        console.log("test")
+        console.log("Hide Ai Generated")
+        document.querySelector('.blurb').parentElement.style.setProperty('display', 'none', 'important');
+    }
+};
+
+// there is probably a neat way to do every search as one function. i'm too lazy to figure it out tonight.
+// have this nightmare of repetition instead
+for (var link of links) {
+    if (link.textContent.trim().toLowerCase().includes('chat gpt')) {
+        console.log("Hide Chat GPT")
+        document.querySelector('.blurb').parentElement.style.setProperty('display', 'none', 'important');
+    }
+};
+
+for (var link of links) {
+    if (link.textContent.trim().toLowerCase().includes('chatgpt')) {
+        console.log("Hide ChatGPT")
+        document.querySelector('.blurb').parentElement.style.setProperty('display', 'none', 'important');
+    }
+};
+
+for (var link of links) {
+    if (link.textContent.trim().toLowerCase().includes('ai fic')) {
+        console.log("Hide ai fic")
         document.querySelector('.blurb').parentElement.style.setProperty('display', 'none', 'important');
     }
 };
